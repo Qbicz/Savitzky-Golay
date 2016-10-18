60e462ecbf821c51d577c97144d775deb995939d
Piotr Palucki <piotrek.palucki@gmail.com>
2016-10-18 23:46:22 +0200

    Added data plotting from txt (CSV) file

--- prototype/savitzky_scipy.py
+++ prototype/savitzky_scipy.py
@@ -4,12 +4,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
+def read_datafile(file_name):
+    data = np.loadtxt(file_name, delimiter=',', unpack=True, skiprows=1)
+    return data
+
+data = read_datafile('../mit-bih-txt/mitdb100.txt')
+
 # time vector
-t = np.arange(0,10,0.1)
+t = data[0]
 
 # ECG vector - put MIT-BIH data here
-ecg = np.random.rand(1,100)*np.cos(t)
-ecg = ecg[0] # extract 1-dim list from 2-dim list
+ecg = data[1]
 
 # Savitzky-Golay filtering
 # http://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.savgol_filter.html
@@ -19,6 +24,7 @@ ecg_filtered = signal.savgol_filter(ecg, 7, 2, deriv=0, delta=1.0, axis=-1, mode
 # do the plotting with pyplot
 plt.plot(t, ecg, 'b-', label="ECG")
 plt.plot(t, ecg_filtered, 'g-', label="ECG filtered")
+plt.plot(t, data[2], 'r-', label="Plain Data[2] from CSV")
 plt.ylabel('ECG signal')
 plt.xlabel('Time [s]')
 plt.legend()
