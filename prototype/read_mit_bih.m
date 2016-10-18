8397bcb1ba6aa84eca6f821113a6f8be222250be
Qbicz <avenger.v14@gmail.com>
2016-10-18 21:11:21 +0200

    ECG data from Physionet in text format

--- prototype/read_mit_bih.m
+++ prototype/read_mit_bih.m
@@ -1,3 +1,10 @@
- %Example 1- Read a signal from PhysioNet's Remote server:
-[tm, signal]=rdsamp('mitdb/100',[],10000); % TODO: whole signal has 30 minutes, find a way to process it quickly
-plot(tm,signal(:,1))
+% Example 1- Read ECG signal from PhysioNet's Remote server:
+[tm, signal]=rdsamp('mitdb/117',[],100000); % TODO: whole signal has 30 minutes, find a way to process it quickly
+%plot(tm,signal(:,1))
+
+% write to text file
+fileID = fopen('mitdb117.txt', 'wb');
+table = [tm signal]';
+fprintf(fileID, 'Time\tMLII [mV]\tV5 [mV]\n');
+fprintf(fileID, '%04.3f\t%0.3f\t%0.3f\n', table);
+fclose(fileID);
