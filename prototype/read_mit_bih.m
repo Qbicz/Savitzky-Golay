5b9f4c6377260b11706b41d74623c042a1bb102f
Piotr Palucki <piotrek.palucki@gmail.com>
2016-10-18 23:14:21 +0200

    Changed format of txt data for CSV. Added howTo for installing WFDB for MATLAB

--- prototype/read_mit_bih.m
+++ prototype/read_mit_bih.m
@@ -1,10 +1,10 @@
 % Example 1- Read ECG signal from PhysioNet's Remote server:
-[tm, signal]=rdsamp('mitdb/117',[],100000); % TODO: whole signal has 30 minutes, find a way to process it quickly
+[tm, signal]=rdsamp('mitdb/101',[],100000); % TODO: whole signal has 30 minutes, find a way to process it quickly
 %plot(tm,signal(:,1))
 
 % write to text file
-fileID = fopen('mitdb117.txt', 'wb');
+fileID = fopen('../mit-bih-txt/mitdb101.txt', 'wb');
 table = [tm signal]';
-fprintf(fileID, 'Time\tMLII [mV]\tV5 [mV]\n');
-fprintf(fileID, '%04.3f\t%0.3f\t%0.3f\n', table);
+fprintf(fileID, 'Time,MLII [mV],V5 [mV]\n');
+fprintf(fileID, '%04.3f,%0.3f,%0.3f\n', table);
 fclose(fileID);
