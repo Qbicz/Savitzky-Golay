0209292440d3c242b357be98cea2ecc58a317bb6
Qbicz <avenger.v14@gmail.com>
2016-11-01 18:52:17 +0100

    treatment of first and last points

--- prototype/savitzky_design.py
+++ prototype/savitzky_design.py
@@ -13,10 +13,10 @@ data = read_datafile('../mit-bih-txt/mitdb100short.txt')
 
 # short test data - 300 samples
 # time vector
-t = data[0][60:80]
+t = data[0][60:100]
 
 # ECG vector
-x = data[1][60:80]
+x = data[1][60:100]
 
 # Savitzky-Golay filtering
 M = 3
@@ -26,9 +26,9 @@ n = 10
 # d is impulse sequence
 d = np.concatenate([np.zeros(M), [1], np.zeros(M)])
 impulse_domain = np.arange(-M,M+1)
-print(d)
+print('d =', d)
 a = np.polyfit(impulse_domain, d, N)
-print(a)
+print('polynomial coefficients a =', a)
 h = np.flipud(np.polyval(a, impulse_domain))
 
 # plot impulse polynomial design
@@ -39,8 +39,19 @@ h = np.flipud(np.polyval(a, impulse_domain))
 #print('Context:', context)
 
 # TODO: perform a convolution from n-M to n+M of sum( h[n-m] * x[n])
-y = x[0:3]
+#y = x[0:3]
 print(y)
+
+# Treatment of first and last points: use 'mirror' extrapolation
+xfirst = np.flipud(x[1:M+1])
+xlast = np.flipud(x[len(x)-M-1:len(x)-1])
+print(xfirst)
+print('x[0:10] =', x[0:10])
+print('...')
+print('x[-10:]', x[-10:])
+print(xlast)
+
+x_mirror = np.concatenate(xfirst, x, xlast)
 
 for n in range(M, len(x)-M):
     impulse_domain = np.arange(n-M,n+M+1)
@@ -51,8 +62,8 @@ for n in range(M, len(x)-M):
     
     # reference
     hr = signal.savgol_coeffs(2*M+1, N, deriv=0, delta=1.0)
-    print('hr: ', hr)
-    print('h:', h)
+    #print('hr: ', hr)
+    #print('h:', h)
     
     for m in range(n-M, n+M):
         #print('n=%d, m=%d' % (n,m))
@@ -68,8 +79,7 @@ for i in range(3):
 ecg_filtered = y
 ecg = x
 
-ecg_ref1 = signal.savgol_filter(ecg, 7, 2, deriv=0, delta=1.0, axis=-1, mode='mirror', cval=0.0)
-ecg_ref2 = signal.savgol_filter(ecg, 7, 2, deriv=0, delta=1.0, axis=-1, mode='interp', cval=0.0)
+ecg_reference = signal.savgol_filter(ecg, 7, 2, deriv=0, delta=1.0, axis=-1, mode='mirror', cval=0.0)
 
 print(len(t), len(x), len(y))
 
@@ -79,8 +89,7 @@ print(len(t), len(x), len(y))
 # plot single heart cycle with 1 QRS
 plt.plot(t, ecg, 'b-', label="ECG")
 plt.plot(t, ecg_filtered, 'g-', label="Savitzky-Golay")
-plt.plot(t, ecg_ref1, 'r-', label="Savitzky-Golay Scipy mirror")
-plt.plot(t, ecg_ref1, 'ko', label="Savitzky-Golay Scipy interp")
+plt.plot(t, ecg_reference, 'ro', label="Sav-Gol SciPy mirror")
 plt.ylabel('ECG signal')
 plt.xlabel('Time [s]')
 plt.legend()
