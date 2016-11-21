9cdafe199609c06e6adc7f456910b6afdb6e0ca0
Piotr Palucki <piotrek.palucki@gmail.com>
2016-11-21 20:25:57 +0100

    Added basic console GUI

--- implem/src/savitzky.cpp
+++ implem/src/savitzky.cpp
@@ -3,8 +3,32 @@
 
 #define MITDBH_FILE "/home/piotr/Studia/ESDMiT_Savitzky-Golay/mit-bih-txt/mitdb100short.txt"
 
+void printHelloMessage()
+{
+    std::cout << " __________________________________________________________\n";
+    std::cout << "||                                                        ||\n";
+    std::cout << "||                               /\\                       ||\n";
+    std::cout << "||           /\\             /\\  /  \\                      ||\n";
+    std::cout << "||    ______/  \\  /\\       /  \\/    \\    __________       ||\n";
+    std::cout << "||              \\/  \\_____/          \\  /                 ||\n";
+    std::cout << "||                                    \\/                  ||\n";
+    std::cout << "||                                                        ||\n";
+    std::cout << "||  Welcome to SavGol - tool for processing ECG signal.   ||\n";
+    std::cout << "||                                                        ||\n";
+    std::cout << "||  What do you want to do?                               ||\n";
+    std::cout << "||  1 -                                                   ||\n";
+    std::cout << "||  2 -                                                   ||\n";
+    std::cout << "||  3 -                                                   ||\n";
+    std::cout << "||                                                        ||\n";
+    std::cout << "||  0 - Exit                                              ||\n";
+    std::cout << "||                                                        ||\n";
+    std::cout << "||________________________________________________________||\n";
+}
+
 int main()
 {
+    printHelloMessage();
+
     MITDbHandler dataHandler;
 
     //Read data from MIT-BIH (parsed to txt: ../../mit-bih-txt/mitdb100short.txt)
